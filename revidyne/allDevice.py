f089bd7ff9847488e34c6dc5cdef0cb61863aac2
Li Bai <lbai@temple.edu>
2023-11-30 11:40:07 -0500

    Update allDevice.py
    add multiple devices with same name, but the speed must be the same.

--- revidyne/allDevice.py
+++ revidyne/allDevice.py
@@ -2,11 +2,37 @@
 from serial.tools import list_ports
 from .serialcom import SerialCommander
 import re, time
+import re
 
+def remove_trailing_number(s):
+    # Use a regular expression to match a trailing number at the end of the string
+    match = re.search(r'\d+$', s)
 
+    if match:
+        # If a match is found, remove the trailing number
+        s = s[:match.start()]
+
+    return s
+    
+def getCount(lst, pat):
+    cnt = 0
+    for i, it in enumerate(lst):
+        if pat in it:
+            cnt += 1
+
+    addelement = pat + str(cnt)
+    modified_elements = []
+    modified_elements.append(addelement)
+    
+    lst.extend(modified_elements)
+
+    return addelement, lst
+    
+    
 class AllDevice:
 
   def __init__(self, spd=9600):
+    self.devicenames=[]
     self.devices={}
     self.spd = spd
     enmu_ports = enumerate(list_ports.comports())
@@ -24,7 +50,8 @@ class AllDevice:
         serial_commander.send_command("*ID?")
         response = serial_commander.read_response()
         print(response)
-
+        response, self.devicenames = getCount(self.devicenames, response) 
+        print(response)
         self.devices[response]=Device(response, port=m, spd=self.spd)            
 
   def getAllDevice(self):
@@ -36,9 +63,10 @@ class Device:
   def __init__(self, name, port, spd=9600):
       self.port = port
       self.name = name
-
+      print(name)
       mod = __import__('.revidyne', fromlist=[name])
-      klass = getattr(mod, name)
+
+      klass = getattr(mod, remove_trailing_number(name))
       self.device=klass(port, spd)
 
   def getDevice(self):
