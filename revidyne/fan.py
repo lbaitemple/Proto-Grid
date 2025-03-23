0fac9a1c13bb1ad72bbc95bfd68f3167abfbec46
Li Bai <lbai@temple.edu>
2025-03-23 00:14:26 -0400

    Update fan.py

--- revidyne/fan.py
+++ revidyne/fan.py
@@ -48,7 +48,7 @@ class fan(HasTraits, SerialCommander):
         while cmd_name != "eoc":
             super(fan, self).send_command(cmd_name)
             cmd_name = super(fan, self).read_response()
-            print(cmd_name)
+            #print(cmd_name)
             num_of_output = 0
             num_of_input = 0
             special_index = -1  
