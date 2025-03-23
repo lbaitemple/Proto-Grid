6575909aed2956a11de6a3578504413080df5b6b
Li Bai <lbai@temple.edu>
2025-03-23 00:13:37 -0400

    Update solartracker.py

--- revidyne/solartracker.py
+++ revidyne/solartracker.py
@@ -54,7 +54,7 @@ class solartracker(HasTraits, SerialCommander):
         while cmd_name != "eoc":
             super(solartracker, self).send_command(cmd_name)
             cmd_name = super(solartracker, self).read_response()
-            print(cmd_name)
+            #print(cmd_name)
             num_of_output = 0
             num_of_input = 0
             special_index = -1  
